bcae890ce7ebf60fbb480cd2e2de50a46a58a297
KingRainGrey <32055007+miles-rush@users.noreply.github.com>
2022-07-03 21:49:12 +0800

    jwt

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,31 @@
 const router = require('koa-router')()
 const User = require('./../models/userSchema')
 const util = require('./../utils/util')
+const jwt = require('jsonwebtoken')
 
 router.prefix('/users')
 
 router.post('/login',async (ctx)=>{
   try {
     const {userName, userPwd} = ctx.request.body; 
+    /**
+     * 返回数据库指定字段 有三种方式
+     * 1.userId userName userEmail state role deptId roleList
+     * 2.{userName:1-返回 0-不返回}
+     * 3.select('userId')
+     */
     const res = await User.findOne({
       userName,
       userPwd
-    })
+    }, 'userId userName userEmail state role deptId roleList')
+    
     if(res) {
-      ctx.body = util.success(res);
+      const data = res._doc;
+      const token = jwt.sign({
+        data: data,
+      },'miles-rush', {expiresIn: '1h'})
+      data.token = token;
+      ctx.body = util.success(data);
     }else{
       ctx.body = util.fail("账号或密码不正确")
     }
